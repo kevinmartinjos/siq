b480f9aac70eb2602f11378e6bf24e5e7ced843e
Kevin Martin Jose <youcancallmekevin@gmail.com>
2017-06-30 14:24:38 +0530

    fixing the example

--- examples/newLogic.js
+++ examples/newLogic.js
@@ -4,16 +4,16 @@ var Siq = require('../src/client/siq');
 var siqConnection = Siq.connect('ws://localhost:8888');
 
 siqConnection.then((siq) => {
-	// var producer = siq.createProducer();
-	// for(var i=0; i<18; i++){
-	// 	producer.produce('newBufferedQueue', 'message');
-	// }
+	var producer = siq.createProducer();
+	for(var i=0; i<18; i++){
+		producer.produce('bufqueue', 'message');
+	}
 
-	// console.log("Messages produced");
+	console.log("Messages produced");
 	var callback = (messageList) => {
 		console.log(messageList.length);
 	};
 
-	var consumerConnection = siq.createConsumer('newBufferedQueue', callback);
+	var consumerConnection = siq.createConsumer('bufqueue', callback);
 	
 });
